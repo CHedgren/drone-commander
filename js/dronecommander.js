f4ac084194308274dcc7db38c3efc57827ba7623
Carl <carl.hedgren@gmail.com>
2015-09-06 22:29:35 +0200

    Started adding knobs

--- js/dronecommander.js
+++ js/dronecommander.js
@@ -1,6 +1,3 @@
-/**
- * Created by Sailor on 22.03.2015.
- */
 var context = new AudioContext();
 
 
@@ -63,25 +60,47 @@ lfo1.connect(lfo1gain);
 filter.connect(peakfilter);
 peakfilter.connect(outamp);
 outamp.connect(context.destination);
+outamp.connect(analyser);
 
 
 var osc1knob =    document.getElementById('controlosc1');
 var mixknob =     document.getElementById('controlmix');
 var osc2knob =    document.getElementById('controlosc2');
-var lfo1knob =    document.getElementById('controllfo');
-var shapeknob =   document.getElementById('controlshape');
 var pulseknob =   document.getElementById('controlpulse');
 var filterknob =  document.getElementById('controlfilter');
 var numbersknob = document.getElementById('control2-4-8-16');
 var volknob =     document.getElementById('vol');
 
+
+var bufferSize = 4096;
+var effect = (function() {
+    var node = context.createScriptProcessor(bufferSize, 1, 1);
+    node.bits = 4; // between 1 and 16
+    node.normfreq = 0.1; // between 0.0 and 1.0
+    var step = Math.pow(1/2, node.bits);
+    var phaser = 0;
+    var last = 0;
+    node.onaudioprocess = function(e) {
+        var input = e.inputBuffer.getChannelData(0);
+        var output = e.outputBuffer.getChannelData(0);
+        for (var i = 0; i < bufferSize; i++) {
+            phaser += node.normfreq;
+            if (phaser >= 1.0) {
+                phaser -= 1.0;
+                last = step * Math.floor(input[i] / step + 0.5);
+            }
+            output[i] = last;
+        }
+    };
+    return node;
+})();
+
+
 osc1.frequency.value = osc1knob.value;
 xfade1.gain.value = mixknob.value/100;
 xfade2.gain.value = mixknob.value/100;
 osc2.frequency.value = osc2knob.value;
-lfo1.frequency.value = lfo1knob.value;
 lfo2gain.gain.value = pulseknob.value/100;
-lfo2.frequency.value = lfo1knob.value*numbersknob.value;
 osc2.frequency.value = osc2knob.value;
 outamp.gain.value = volknob.value/100;
 
@@ -92,7 +111,6 @@ outamp.gain.value = volknob.value/100;
 osc1knob.addEventListener("input", slide);
 mixknob.addEventListener("input", slide);
 osc2knob.addEventListener("input", slide);
-lfo1knob.addEventListener("input", slide);
 pulseknob.addEventListener("input", slide);
 numbersknob.addEventListener("input", slide);
 osc2.addEventListener("input", slide);
@@ -118,7 +136,7 @@ function slide() {
     if (this.getAttribute("id") == 'controlosc1') {
 
         osc1.frequency.value = this.value;
-
+        console.log("osc1: ", osc1.frequency.value);
     }
 
     else if (this.getAttribute("id") == 'controlmix') {
@@ -198,6 +216,52 @@ function shapecoefficients(t, a) {
 
 
 
+analyser.fftSize = 2048;
+var bufferLength = analyser.frequencyBinCount;
+var dataArray = new Uint8Array(bufferLength);
+analyser.smoothingTimeConstant = 1;
+analyser.getByteTimeDomainData(dataArray);
+analyser.maxDecibels =500;
 
 
 
+// draw an oscilloscope of the current audio source
+var canvas = document.getElementById('drone-control');
+var canvasCtx = canvas.getContext('2d');
+
+function draw() {
+
+    drawVisual = requestAnimationFrame(draw);
+
+    analyser.getByteTimeDomainData(dataArray);
+
+    canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+    canvasCtx.fillRect(0, 0, canvas.width, canvas.height);
+
+    canvasCtx.lineWidth = 2;
+    canvasCtx.strokeStyle = 'rgb(0, 0, 0)';
+
+    canvasCtx.beginPath();
+
+    var sliceWidth = canvas.width * 1.0 / (bufferLength);
+    var x = 0;
+
+    for(var i = 0; i < bufferLength; i++) {
+
+        var v = dataArray[i] / (128.0);
+        var y = v * canvas.height/2 ;
+
+        if(i === 0) {
+            canvasCtx.moveTo(x, y);
+        } else {
+            canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+    }
+
+    canvasCtx.lineTo(canvas.width, canvas.height/2);
+    canvasCtx.stroke();
+};
+
+draw();
